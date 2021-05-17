3492b82b5903fe6b7eeb0b1c3c38064fce4a8b27
AviMegin <82816950+AviMegin@users.noreply.github.com>
2021-05-17 13:50:54 +0200

    Create testconnect.php

---
+++ Chatbot/testconnect.php
@@ -0,0 +1,7 @@
+<?php
+$dbhost = "localhost";
+ $dbuser = "id16802500_boti";
+ $dbpass = "a-n?6>u~^<dV[I5M";
+ $db = "id16802500_bot";
+ $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+ ?>
