bd67e21ee897a6abdb95f3454cfb3224a8ed02f8
AviMegin <82816950+AviMegin@users.noreply.github.com>
2021-05-31 17:02:05 +0200

    oops

---
+++ Voting/db_connect.php
@@ -0,0 +1,7 @@
+<?php
+$dbhost = "localhost";
+ $dbuser = "id16802500_voting";
+ $dbpass = "v0c6*ITpWEZB3#xE";
+ $db = "id16802500_voting_db";
+ $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+ ?>
