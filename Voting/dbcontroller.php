12bdf35df644c634e71fd550ead182871951632b
AviMegin <82816950+AviMegin@users.noreply.github.com>
2021-06-06 12:30:22 +0200

    oh gosh

---
+++ Voting/dbcontroller.php
@@ -0,0 +1,43 @@
+<?php
+
+class DBController {
+	private $host = "localhost";
+	private $user = "id16942327_vote";
+	private $password = "F|d(7z5Bj1rq*0oF";
+	private $database = "id16942327_poll";
+	private $conn;
+
+	function __construct() {
+		$this->conn = $this->connectDB();
+	}
+
+	function connectDB() {
+		$conn = mysqli_connect($this->host,$this->user,$this->password,$this->database);
+		return $conn;
+	}
+
+	function runQuery($query) {
+		$result = mysqli_query($this->conn,$query);
+		while($row=mysqli_fetch_array($result)) {
+			$resultset[] = $row;
+		}
+		if(!empty($resultset))
+			return $resultset;
+	}
+
+	function insertQuery($query) {
+	    mysqli_query($this->conn, $query);
+	    $insert_id = mysqli_insert_id($this->conn);
+	    return $insert_id;
+	}
+
+	function getIds($query) {
+	    $result = mysqli_query($this->conn,$query);
+	    while($row=mysqli_fetch_array($result)) {
+	        $resultset[] = $row[0];
+	    }
+	    if(!empty($resultset))
+	        return $resultset;
+	}
+}
+?>
--- Voting/index.php
+++ Voting/index.php
@@ -1,131 +1,55 @@
-<!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-  <title>Voting System</title>
- 	
-
-<?php
-	session_start();
-  if(!isset($_SESSION['login_id']))
-    header('location:login.php');
- include('./header.php'); 
- // include('./auth.php'); 
- ?>
-
+<title>PHP Poll Script</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
 </head>
-<style>
-	body{
-        background: #80808045;
-  }
-</style>
-
 <body>
-	<?php include 'topbar.php' ?>
-	<?php include 'navbar.php' ?>
-  <div class="toast" id="alert_toast" role="alert" aria-live="assertive" aria-atomic="true">
-    <div class="toast-body text-white">
-    </div>
-  </div>
-  <main id="view-panel" >
-      <?php $page = isset($_GET['page']) ? $_GET['page'] :'home'; ?>
-  	<?php include $page.'.php' ?>
-  	
-
-  </main>
-
-  <div id="preloader"></div>
-  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
-
-<div class="modal fade" id="confirm_modal" role='dialog'>
-    <div class="modal-dialog modal-md" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-        <h5 class="modal-title">Confirmation</h5>
-      </div>
-      <div class="modal-body">
-        <div id="delete_content"></div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" id='confirm' onclick="">Continue</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-      </div>
-    </div>
-  </div>
-  <div class="modal fade" id="uni_modal" role='dialog'>
-    <div class="modal-dialog modal-md" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-        <h5 class="modal-title"></h5>
-      </div>
-      <div class="modal-body">
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" id='submit' onclick="$('#uni_modal form').submit()">Save</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-      </div>
-      </div>
-    </div>
-  </div>
-</body>
+	<div id="overlay">
+		<div>
+			<img src="loader.gif" width="64px" height="64px" />
+		</div>
+	</div>
+	<div class="poll-content-outer">
+		<div id="poll-content"></div>
+	</div>
+<script src="jquery-3.2.1.min.js"></script>
 <script>
-	 window.start_load = function(){
-    $('body').prepend('<di id="preloader2"></di>')
-  }
-  window.end_load = function(){
-    $('#preloader2').fadeOut('fast', function() {
-        $(this).remove();
-      })
-  }
-
-  window.uni_modal = function($title = '' , $url=''){
-    start_load()
-    $.ajax({
-        url:$url,
-        error:err=>{
-            console.log()
-            alert("An error occured")
-        },
-        success:function(resp){
-            if(resp){
-                $('#uni_modal .modal-title').html($title)
-                $('#uni_modal .modal-body').html(resp)
-                $('#uni_modal').modal('show')
-                end_load()
-            }
-        }
-    })
-}
-window._conf = function($msg='',$func='',$params = []){
-     $('#confirm_modal #confirm').attr('onclick',$func+"("+$params.join(',')+")")
-     $('#confirm_modal .modal-body').html($msg)
-     $('#confirm_modal').modal('show')
-  }
-   window.alert_toast= function($msg = 'TEST',$bg = 'success'){
-      $('#alert_toast').removeClass('bg-success')
-      $('#alert_toast').removeClass('bg-danger')
-      $('#alert_toast').removeClass('bg-info')
-      $('#alert_toast').removeClass('bg-warning')
-
-    if($bg == 'success')
-      $('#alert_toast').addClass('bg-success')
-    if($bg == 'danger')
-      $('#alert_toast').addClass('bg-danger')
-    if($bg == 'info')
-      $('#alert_toast').addClass('bg-info')
-    if($bg == 'warning')
-      $('#alert_toast').addClass('bg-warning')
-    $('#alert_toast .toast-body').html($msg)
-    $('#alert_toast').toast({delay:3000}).toast('show');
-  }
-  $(document).ready(function(){
-    $('#preloader').fadeOut('fast', function() {
-        $(this).remove();
-      })
-  })
-</script>	
+   function show_poll(){
+		$.ajax({
+			type: "POST", 
+			url: "show-poll.php", 
+			processData : false,
+			beforeSend: function() {
+				$("#overlay").show();
+			},
+			success: function(responseHTML){
+				$("#overlay").hide();
+				$("#poll-content").html(responseHTML);
+			}
+		});
+	}
+	function addPoll() {
+		if($("input[name='answer']:checked").length != 0){
+			var answer = $("input[name='answer']:checked").val();
+			$.ajax({
+				type: "POST", 
+				url: "save-poll.php", 
+				data : "question="+$("#question").val()+"&answer="+$("input[name='answer']:checked").val(),
+				processData : false,
+				beforeSend: function() {
+					$("#overlay").show();
+				},
+				success: function(responseHTML){
+					$("#overlay").hide();	
+					$("#poll-content").html(responseHTML);				
+				}
+			});
+			
+		}
+	}
+    $(document).ready(function(){
+		show_poll();
+	});
+   </script>
+</body>
 </html>
