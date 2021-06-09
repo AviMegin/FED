8e752a92312934997edf739854510c2a0c7f400f
AviMegin <82816950+AviMegin@users.noreply.github.com>
2021-06-09 22:48:22 +0200

    Update contact.php

--- php custmoer service/contact.php
+++ php custmoer service/contact.php
@@ -14,7 +14,7 @@
   <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <link
     rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
 
 
       </head>
