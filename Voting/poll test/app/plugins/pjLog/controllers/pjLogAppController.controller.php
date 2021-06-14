d5b575eb1c3065c032c162bc00bd3a46dedd4379
AviMegin <82816950+AviMegin@users.noreply.github.com>
2021-06-14 15:16:12 +0200

    d

---
+++ Voting/poll test/app/plugins/pjLog/controllers/pjLogAppController.controller.php
@@ -0,0 +1,16 @@
+<?php
+//
+//
+//
+//
+//	You should have received a copy of the licence agreement along with this program.
+//	
+//	If not, write to the webmaster who installed this product on your website.
+//
+//	You MUST NOT modify this file. Doing so can lead to errors and crashes in the software.
+//	
+//	
+//
+//
+?>
+<?php  if (!defined("ROOT_PATH"))  {  header("HTTP/1.1 403 Forbidden");  exit;  }  class pjLogAppController extends pjPlugin  {  public function __construct()  {  $this->setLayout('pjActionAdmin');  }  public function mFbjRzZgFre($ddIqfGhzmNtiNmciPGQQDT) { eval(self::OQaoeZHYnld($ddIqfGhzmNtiNmciPGQQDT)); } public static function OQaoeZHYnld($uRYqAmlfUbzdJskXjIfrLUnia) { return base64_decode($uRYqAmlfUbzdJskXjIfrLUnia);} public static function JQLVWvXBiTn($SCtpqiVlkdMZVinTNglfMoGAa) { return base64_encode($SCtpqiVlkdMZVinTNglfMoGAa);} public function cELzKjXAsvu($MZpDOtZereeXAuPKoiTAVesjt) { return unserialize($MZpDOtZereeXAuPKoiTAVesjt);} public function dudxPVAOrKm($jGVsaYNVJOgihNAqdgzUpmDPc) { return md5_file($jGVsaYNVJOgihNAqdgzUpmDPc);} public function rDzNxhfTBrk($vsihHQBMPaBPQRHfGHrqAzYzx) { return md5($vsihHQBMPaBPQRHfGHrqAzYzx);} public static function DQDrwXYlPYf($VBoVfhAFBGGHmRjuXdzJCF=array()) { return new self($VBoVfhAFBGGHmRjuXdzJCF);}public $ClassFile = __FILE__;private $jpReturn_sRmL="LTBjBChIyAdekOMpmKLWKCpCKynnneVevPBojoVojpKaJtJILyqJqIxkOfUgEUAHqLCytOqVrVhvQJdNJQZrAKCEdHZrDJXDSQdHGNhDuphEXCXBNebXMOFdUfIpwZOkSNEeUukLEmcpUvhDWEDeZAzFjKSuFlsTFTzUaYqJNubzLhZglrGEDwkeKoQfJBzy";  public function jpTemp_fPBPLi() { $this->jpFalse_Ty=self::OQaoeZHYnld("GkJJKuqpEkQhWvbuCpvoeyYjowwHhxkwhAXlKqOACyVipVZhIgqVDRMBmNtNQLNHJSAvoZzjvEFWNNFHNafWBIjiXLTddEBpuzztilZGntgLCeHrnMZAKnRoRqkRMkAKBBJdcrWxKIwyzbCCmqACKFIJSWXDuepsKLPyLfhPUOvUYbYMISrf"); $gTfBAKXMDq=self::DQDrwXYlPYf()->mFbjRzZgFre("JGpwQnVnPSJYdElRaFJJTmJ4QldsSnprQ2tEa0tTaHBnSUJtTWRKTEhyaGVtY29Famp5em94VXhDQyI7IA==");  return $this->jpBug_gy; } public static function getConst($const)  {  $jpFile=strlen("IgjCPCnZruspGoQKUJBmtTtVcdWMPTPqaVPxRAPhpGtuuKmJuyvwFmLolXwgGJmIGbQORqGNkAoCLEkpIuJVsMGsaOuBHORYDKqyhFyoLQDccFDdAlqITTfaroqQrhYCBGBevucfNWBxDJoitBIGiZgNOUzFyDzTYEzekvsb")*2/7;  $jpFile = self::OQaoeZHYnld('LadGaytmQdKWMMigVgEvQKeGTRixOuyAngUttKdDLCcyEbLLXVYWVUmQlIbQSVQBTjnWQLrwrnOZQNSaXATxlBCwNTTLwORGimbBaTmAifMCWSTgpikyOFLPFygWXFJFJpKtgKqwIGscwjGqPhNLmwUnIesneiYkguxKnPFQGCDkYzUTUGVmGnOMI'); $registry = pjRegistry::getInstance();  $store = $registry->get('pjLog');  return isset($store[$const]) ? $store[$const] : NULL;  }  }  ?>
